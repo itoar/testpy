17dfa2219c92b011253967d341ad9dc42a8cc5d7
itoar <aekn1127@icloud.com>
2024-04-02 23:23:21 +0900

    test

--- sc/test2.py
+++ sc/test2.py
@@ -236,3 +236,48 @@ def processWeight(K=2):
             tmp_list = np.array(i)
             list = np.vstack([list, tmp_list])
     print(list)
+
+
+
+from PySide2 import QtWidgets, QtCore, QtGui
+
+class Window(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent=parent)
+
+        layout = QtWidgets.QVBoxLayout()
+
+        self.treewidget = QtWidgets.QTreeWidget()
+        self.treewidget.setColumnCount(4)
+        layout.addWidget(self.treewidget)
+
+        self.setLayout(layout)
+
+        self.build_tree()
+
+
+    def build_tree(self):
+        for index in range(10):
+            item = QtWidgets.QTreeWidgetItem(self.treewidget)
+            item.setText(0, 'icon 1')
+            item.setText(1, 'icon 2')
+
+            line_edit = QtWidgets.QLineEdit(self.treewidget)
+            push_button = QtWidgets.QPushButton(self.treewidget)
+            push_button.setText('TEST')
+            push_button.clicked.connect(self.the_button_was_clicked)
+            slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+
+            self.treewidget.setItemWidget(item, 2, push_button)
+            self.treewidget.setItemWidget(item, 3, slider)
+            
+
+    def the_button_was_clicked(self):
+        num = self.treewidget.topLevelItemCount()
+        for i in range(num):
+            item = self.treewidget.topLevelItem(i)
+            slide = self.treewidget.itemWidget(item, 3)
+            print(f"i:{i} val:{slide.value()}")
+
+window = Window()
+window.show()
